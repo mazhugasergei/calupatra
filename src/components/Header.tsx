2ca827d9892cc2cd90e43e7a2066d9bd837a4f76
Markus Wedler <markuswedler8@gmail.com>
2023-08-29 17:22:37 +0900

    Improve menu btn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 // react
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 // react-router-dom
 import { Link } from "react-router-dom"
 
 export default () => {
   const headerRef = useRef<HTMLElement>(null)
+  const [menuOpened, setMenuOpened] = useState(false)
 
   useEffect(()=>{
     document.addEventListener('scroll', ()=>{
@@ -15,7 +16,13 @@ export default () => {
   return (
     <header ref={headerRef}>
       <Link to="/" className="logo">Calupatra</Link>
-      <Link to="/" className="btn">Get started</Link>
+      <button className={`menu-btn ${menuOpened ? "menu-opened" : "menu-closed"}`} onClick={() => setMenuOpened(prevState => !prevState)}>
+        <div className="lines-cont">
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+        </div>
+      </button>
     </header>
   )
 }
