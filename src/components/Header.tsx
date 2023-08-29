f236c1e5ea59a41f99272a2074a14ae553983182
Markus Wedler <markuswedler8@gmail.com>
2023-08-29 18:48:29 +0900

    Add menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,9 +13,31 @@ export default () => {
     })
   }, [])
 
+  useEffect(()=>{
+    document.body.style.overflow = menuOpened ? "hidden" : "unset"
+  }, [menuOpened])
+
   return (
     <header ref={headerRef}>
+      {/* Logo */}
       <Link to="/" className="logo">Calupatra</Link>
+
+      {/* Nav / Menu */}
+      <nav className={`${menuOpened ? "menu-opened" : "menu-closed"}`}>
+        <ul>
+          <li>
+            <a href="/">Link One</a>
+          </li>
+          <li>
+            <a href="/">Link Two</a>
+          </li>
+          <li>
+            <a href="/">Link Three</a>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Menu btn */}
       <button className={`menu-btn ${menuOpened ? "menu-opened" : "menu-closed"}`} onClick={() => setMenuOpened(prevState => !prevState)}>
         <div className="lines-cont">
           <div className="line" />
